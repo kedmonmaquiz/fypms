4de1ecd9e546fe72ffe457109b64dfc348fa1f9c
Dorcas <leskangad@gmail.com>
2021-02-17 15:10:02 +0300

    add the de

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DepartmentRequest;
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $departments = Department::all();
+        return view('departments.index',compact('departments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DepartmentRequest $request)
+    {
+        $inputs = $request->all();
+        $department = Department::create($inputs);
+        return redirect()->back()->with('message','New Department Added Successfully!!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Department $department)
+    {
+        return view('departments.show',compact('department'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Department $department)
+    {
+        return view('departments.edit',compact('department'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DepartmentRequest $request, Department $department)
+    {
+       $inputs = $request->all();
+       $department->update($inputs);
+       return redirect()->route('departments.index')->with('message','One Department updated Successfully!!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Department $department)
+    {
+        $department->delete();
+        return redirect()->back()->with('message','Department removed Successfully!!');
+    }
+}
