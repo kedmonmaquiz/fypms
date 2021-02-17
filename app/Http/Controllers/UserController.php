5b2b10df526184bec15eca540ca54ba17dfccd34
mozee95 <mosesrumanyika@gmail.com>
2021-02-17 14:49:12 +0300

    add the UserController.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\AcademicYear;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $activeAcademicYearId = AcademicYear::where('status',1)->first()->id;
+
+        $user = User::create([
+            'username'=>$request->username,
+            'first_name'=>$request->first_name,
+            'middle_name'=>$request->middle_name,
+            'last_name'=>$request->last_name,
+            'email'=>$request->email,
+            'phone'=>$request->phone,
+            'gender'=>$request->gender,
+            'department_id'=>$request->department_id,
+            'program_id'=>$request->program_id,
+            'password' => Hash::make(strtoupper($request->last_name)),
+            'academic_year_id'=>$activeAcademicYearId,
+        ]);
+
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('users.index')->with('message','One user added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        $inputs = $request->all();
+        $user->update($inputs);
+
+        $user->roles()->sync($request->roles);
+        return redirect()->route('users.index')->with('message','One user updated successfully');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('message','One user deleted successfully');
+    }
+}
