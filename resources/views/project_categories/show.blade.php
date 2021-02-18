38631006f17e4907af4e80c93b2e26dd3bb3b9de
Dorcas <leskangad@gmail.com>
2021-02-18 16:53:37 +0300

    add ProjectCategories.Views.php

---
+++ resources/views/project_categories/show.blade.php
@@ -0,0 +1,54 @@
+
+
+ @extends('layouts.main')
+ @section('title','Project Category')
+ @section('content-header')
+  
+ @endsection
+
+ @section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <div class="box box-primary">
+                    <div class="box-body">
+                       <table class="table table-primary table-bordered">
+                          <tr>
+                            <th>Attribute</th>
+                            <th>Value</th>
+                          </tr>
+                          <tr>
+                            <td>Name</td>
+                            <td>{{$projectCategory->name}}</td>
+                          </tr>
+
+                          <tr>
+                            <td>Display Name</td>
+                            <td>{{$projectCategory->display_name}}</td>
+                          </tr>
+                    
+                          <tr>
+                            <td>Description</td>
+                            <td>@if($projectCategory->description) {{$projectCategory->description}} @else <i class="text-danger">Not Found</i>@endif</td>
+                          </tr>
+                       </table>
+                    </div>
+                    
+                </div>
+                <!-- /.box -->
+            </div>
+        </div>
+ @endsection
+
+ @section('js')
+  <script>
+   
+  </script>
+ <script>
+   
+    $(function(){
+      
+    });
+ </script>
+ 
+ @endsection
+
