1007567c6cdd4b3309bddcfe3af57e77ff75e9f0
sonrandu <sonrandu.net@gmail.com>
2021-02-19 08:06:37 +0300

    Supervisor home view added

---
+++ resources/views/supervisor/home.blade.php
@@ -0,0 +1,111 @@
+<div class="row">
+  
+  <div class="col-md-3">
+		<!-- small box -->
+      <div class="small-box bg-aqua">
+        <div class="inner">
+          <h3>{{\App\Models\User::all()->count()}}</h3>
+
+          <p>Supervisee</p>
+        </div>
+        <div class="icon">
+          <i class="fa fa-users"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+          More info <i class="fa fa-arrow-circle-right"></i>
+        </a>
+      </div>
+  </div>
+
+	<div class="col-md-3">
+<!-- small box -->
+    <div class="small-box bg-aqua">
+      <div class="inner">
+        <h3>{{\App\Models\Project::all()->count()}}</h3>
+
+        <p>Projects</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-tasks"></i>
+      </div>
+      <a href="#" class="small-box-footer">
+        More info <i class="fa fa-arrow-circle-right"></i>
+      </a>
+    </div>
+	</div>
+
+	<div class="col-md-3">
+		<!-- small box -->
+    <div class="small-box bg-aqua">
+      <div class="inner">
+        <h3>{{\App\Models\Consultation::all()->count()}}</h3>
+        <p>Consultations</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-commenting"></i>
+      </div>
+      <a href="#" class="small-box-footer">
+        More info <i class="fa fa-arrow-circle-right"></i>
+      </a>
+    </div>
+	</div>
+
+	<div class="col-md-3">
+		<!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>{{\App\Models\College::all()->count()}}</h3>
+
+              <p>Reports</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-file-pdf-o"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+	</div>
+</div>
+
+<div class="row">
+  <div class="col-md-6">
+     <div class="box box-default box-solid">
+      <div class="box-header with-border">
+        <h3 class="box-title">Projects According to Categories</h3>
+
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+        </div>
+        <!-- /.box-tools -->
+      </div>
+    <!-- /.box-header -->
+      <div class="box-body">
+        <div id="barchart1" style="width: 100%;height: 400px;"></div>
+     </div>
+    <!-- /.box-body -->
+    </div>
+  </div>
+
+  <div class="col-md-6">
+     <!-- Pie chart -->
+    <div class="box box-default box-solid">
+      <div class="box-header with-border">
+        <h3 class="box-title">Projects According to Categories</h3>
+
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+        </div>
+        <!-- /.box-tools -->
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <div id="piechart1" style="width: 100%;height: 400px;"></div>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!--. Pie chart -->
+  </div>
+</div>
